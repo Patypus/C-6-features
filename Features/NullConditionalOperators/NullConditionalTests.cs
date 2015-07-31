ad3fbb4675134065e4e4609adf9b8c008b853027
Pat Howell <patrick.howell@outlook.com>
2015-07-31 21:46:35 +0100

    Improved creating PersonFilters to use in test and improved test names to explain what is being tested.

--- Features/NullConditionalOperators/NullConditionalTests.cs
+++ Features/NullConditionalOperators/NullConditionalTests.cs
@@ -11,13 +11,20 @@ namespace Features.NullConditionalOperators
     [TestFixture]
     public class NullConditionalTests
     {
+        private PersonFilters _filters;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _filters = new PersonFilters();
+        }
+        
         [Test]
-        public void GetRoadAndNumberDetails_ReturnsExpectedElementsFromTestData()
+        public void NullConditionalOperatorAndWhereClause_GiveExpectedResult()
         {
             var testDataCollection = CreateTestPeople();
-            var filters = new PersonFilters();
 
-            var result = filters.GetHouseAndRoadDetailsWherePresent(testDataCollection);
+            var result = _filters.GetHouseAndRoadDetailsWherePresent(testDataCollection);
             var expected = new List<string> { "25 Somewhere Street", "12 Another Road" };
 
             CollectionAssert.AreEquivalent(expected, result);
@@ -27,9 +34,8 @@ namespace Features.NullConditionalOperators
         public void NullCoalescingOperatorSubstituesMissingPropertyValue()
         {
             var testDataCollection = CreateTestPeople();
-            var filters = new PersonFilters();
 
-            var results = filters.CollectFirstNamesWithNullCoalescing(testDataCollection);
+            var results = _filters.CollectFirstNamesWithNullCoalescing(testDataCollection);
             var substituedValues = results.Where(n => n == "Unknown");
 
             Assert.AreEqual(2, substituedValues.Count());
@@ -38,27 +44,22 @@ namespace Features.NullConditionalOperators
         [Test]
         public void NamesByDateCheck_ReturnsNullWhenParameterIsNull()
         {
-            var filters = new PersonFilters();
-
-            Assert.IsNull(filters.CollectNamesByDateWithNullCheckOnParameter(null, DateTime.Now));
+            Assert.IsNull(_filters.CollectNamesByDateWithNullCheckOnParameter(null, DateTime.Now));
         }
 
         [Test]
         public void NamesByDateCheck_ReturnsExpectedNamesWhenParameterIsNotNull()
         {
             var testDataCollection = CreateNamedAndDatedPeople();
-            var filters = new PersonFilters();
 
-            var results = filters.CollectNamesByDateWithNullCheckOnParameter(testDataCollection, new DateTime(2012, 1, 1));
+            var results = _filters.CollectNamesByDateWithNullCheckOnParameter(testDataCollection, new DateTime(2012, 1, 1));
             var expected = new List<string> { "Amy Hill", "Alison Smith" };
         }
 
         [Test]
         public void EmptyCollectionReturnedWhenParameterIsNull_WithNullCoalescingCheckOnFilterResult()
         {
-            var filters = new PersonFilters();
-
-            var result = filters.CollectNamesByDate_ReturningEmptyCollectionFullNullParameter(null, DateTime.Now);
+            var result = _filters.CollectNamesByDate_ReturningEmptyCollectionFullNullParameter(null, DateTime.Now);
             Assert.AreEqual(0, result.Count());
         }
         
