0457ecd5551fd82ca3086a99b25b022496f18f40
Pat Howell <patrick.howell@outlook.com>
2015-08-03 21:33:27 +0100

    Added examples for auto properties

---
+++ Features/AutoProperties/AutoPropertiesTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.AutoProperties
+{
+    [TestFixture]
+    public class AutoPropertiesTests
+    {
+        [Test]
+        public void ConstructorSetterSetsPropertyValue()
+        {
+            var customerName = "Clark Kent";
+            var item = new AutoPropertyOrderItem(customerName, 12, 1);
+
+            Assert.AreEqual(customerName, item.Customer);
+        }
+
+        [Test]
+        public void DefaultValueIsReturnedByGetIfOtherValueIsNotSet()
+        {
+            var item = new AutoPropertyOrderItem();
+
+            Assert.AreEqual("unknown", item.Customer);
+        }
+
+        // This is a compile failure too as Customer only exposes a getter and has no setter outside the 
+        // automatically added one available to the constructor of AutoPropertyOrderItem.
+        //
+        //[Test]
+        //public void CannotGiveValueToPropertyWithNoSetter()
+        //{
+        //    var item = new AutoPropertyOrderItem();
+
+        //    item.Customer = "illegal update";
+        //}
+    }
+}
---
+++ Features/AutoProperties/AutoPropertyOrderItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.AutoProperties
+{
+    public class AutoPropertyOrderItem
+    {
+        public string Customer { get; } = "unknown";
+
+        public int ItemCode { get; }
+
+        public int Quantity { get; } = -1;
+
+        public AutoPropertyOrderItem(string orderingCustomer = "", int orderItemCode = -1, int quantityOfItem = 0)
+        {
+            if (!string.IsNullOrEmpty(orderingCustomer))
+            {
+                //This is the clever bit for the auto property, it automatically has a setter the only constructor can access.
+                Customer = orderingCustomer;
+            }
+            if (orderItemCode >= 0)
+            {
+                ItemCode = orderItemCode;
+            }
+            if (quantityOfItem > 0)
+            {
+                Quantity = quantityOfItem;
+            }
+        }
+        
+        /*
+        * This is a compile failure are the auto setter is only available to the constructor.
+        *
+        * public void SetCustomer(string newCustomer)
+        * {
+        *     Customer = newCustomer;
+        * }
+        */
+    }
+}
