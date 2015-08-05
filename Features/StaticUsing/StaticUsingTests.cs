0e419c1a376688d597064c41c27b05af63ad6941
Pat Howell <patrick.howell@outlook.com>
2015-08-05 21:47:34 +0100

    Added examples for static usings.

---
+++ Features/StaticUsing/StaticUsingTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Features.StaticUsing.StaticUtilities;
+using static Features.StaticUsing.Mood;
+
+namespace Features.StaticUsing
+{
+    [TestFixture]
+    public class StaticUsingTests
+    {
+        /**
+        * There is nothing really that special to test here, it is just mainly to see how it compiles.
+        */
+
+        [Test]
+        public void CheckPositiveMoods()
+        {
+            var mood = Cheerful;
+            var positiveMoods = PositiveMoodsCollection();
+
+            Assert.IsTrue(positiveMoods.Contains(mood));
+        }
+
+        [Test]
+        public void CheckNeutralList()
+        {
+            var mood = Meh;
+            var neutralMoods = NeutralMoodsCollection();
+
+            Assert.IsTrue(neutralMoods.Contains(mood));
+        }
+    }
+}
---
+++ Features/StaticUsing/StaticUtilities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Features.StaticUsing.Mood;
+
+namespace Features.StaticUsing
+{
+    public static class StaticUtilities
+    {
+        /**
+        * The static using is in here to refer to enum values directly though it looses some context.
+        */
+
+        public static IEnumerable<Mood> PositiveMoodsCollection()
+        {
+            return new List<Mood>
+            {
+                Ecstatic,
+                Happy,
+                Cheerful
+            };
+        }
+
+        public static IEnumerable<Mood> NeutralMoodsCollection()
+        {
+            return new List<Mood>
+            {
+                Alright,
+                YeahYouKnow,
+                Meh
+            };
+        }
+    }
+}
