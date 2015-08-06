48d091515b42ef8893bae9b6615b608c0bb3373a
Pat Howell <patrick.howell@outlook.com>
2015-08-06 21:49:50 +0100

    Cleaned up unused usings

--- Features/StringInterpolation/StringFormatter.cs
+++ Features/StringInterpolation/StringFormatter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Features.StringInterpolation
+﻿namespace Features.StringInterpolation
 {
     public class StringFormatter
     {
