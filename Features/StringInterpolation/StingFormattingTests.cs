73b576b78d17b628a7968c6ee869477d1e560305
Pat Howell <patrick.howell@outlook.com>
2015-08-06 21:49:13 +0100

    Added some more complex tests for string interpolation

--- Features/StringInterpolation/StingFormattingTests.cs
+++ Features/StringInterpolation/StingFormattingTests.cs
@@ -37,5 +37,23 @@ namespace Features.StringInterpolation
             var result = _formatter.FormatGreeting(name, day);
             Assert.AreEqual("Hello Mr \"Alias\", how are you this day?", result);
         }
+
+        [Test]
+        public void FormatWorksWithPathSlashes()
+        {
+            var path = @"C:\Somewhere\over\the\rainbow.txt";
+            var result = _formatter.FormatFileLocationMessage(path);
+
+            Assert.AreEqual(@"The file you want is located at: C:\Somewhere\over\the\rainbow.txt", result);
+        }
+
+        [Test]
+        public void FormatWorksWithBracketsInString()
+        {
+            var message = "hello world";
+            var result = _formatter.FormatMessageWithBrackets(message);
+
+            Assert.AreEqual("Your message hello world, {these are escaped}", result);
+        }
     }
 }
--- Features/StringInterpolation/StringFormatter.cs
+++ Features/StringInterpolation/StringFormatter.cs
@@ -12,5 +12,15 @@ namespace Features.StringInterpolation
         {
             return $"Hello {name}, how are you this {day}?";
         }
+
+        public string FormatFileLocationMessage(string pathLocation)
+        {
+            return $"The file you want is located at: {pathLocation}";
+        }
+
+        public string FormatMessageWithBrackets(string message)
+        {
+            return $"Your message {message}, {{these are escaped}}";
+        }
     }
 }
