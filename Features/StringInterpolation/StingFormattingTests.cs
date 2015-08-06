ceae0b60ac9b3991b536180455081267984bffe4
Pat Howell <patrick.howell@outlook.com>
2015-08-06 21:41:21 +0100

    Added simple string interpolation formatting example

---
+++ Features/StringInterpolation/StingFormattingTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.StringInterpolation
+{
+    [TestFixture]
+    public class StingFormattingTests
+    {
+        private StringFormatter _formatter;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            _formatter = new StringFormatter();
+        }
+
+        [Test]
+        public void FormatGreetingInsertsParameters()
+        {
+            var name = "Fred";
+            var day = "Thursday";
+
+            var result = _formatter.FormatGreeting(name, day);
+            Assert.AreEqual("Hello Fred, how are you this Thursday?", result);
+        }
+
+        [Test]
+        public void FormateWithMoreComplexParameter()
+        {
+            var name = "Mr \"Alias\"";
+            var day = "day";
+
+            var result = _formatter.FormatGreeting(name, day);
+            Assert.AreEqual("Hello Mr \"Alias\", how are you this day?", result);
+        }
+    }
+}
---
+++ Features/StringInterpolation/StringFormatter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.StringInterpolation
+{
+    public class StringFormatter
+    {
+        public string FormatGreeting(string name, string day)
+        {
+            return $"Hello {name}, how are you this {day}?";
+        }
+    }
+}
