0b1a436ab0de557364d9ecb23a3943a457280906
Pat Howell <patrick.howell@outlook.com>
2015-08-10 22:24:54 +0100

    Added simple example of index initialisation

---
+++ Features/IndexInitialisers/InitialisationFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.IndexInitialisers
+{
+    public static class InitialisationFactory
+    {
+        /*
+        * This example uses the dictionary as the indexed collection.
+        */
+        public static IDictionary<string, Record> CreateTestRecords()
+        {
+            var recordOne = new Record { Name = "Athos", DateOfBirth = new DateTime(1615, 12, 3), Email = "athos@kingsguard.fr" };
+            var recordTwo = new Record { Name = "Porthos", DateOfBirth = new DateTime(1620, 7, 14), Email = "porthos@kingsguard.fr" };
+            var recordThree = new Record { Name = "Aramis", DateOfBirth = new DateTime(1618, 8, 23), Email = "aramis@kingsguard.fr" };
+
+            return new Dictionary<string, Record>
+            {
+                ["muskateerOne"] = recordOne,
+                ["muskateerTwo"] = recordTwo,
+                ["muskateerThree"] = recordThree
+            };
+        }
+    }
+}
---
+++ Features/IndexInitialisers/InitialisationTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using static Features.IndexInitialisers.InitialisationFactory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features.IndexInitialisers
+{
+    [TestFixture]
+    public class InitialisationTests
+    {
+        [Test]
+        public void TestInitialisedDictionary()
+        {
+            var createdDictionary = CreateTestRecords();
+
+            Assert.AreEqual("Athos", createdDictionary["muskateerOne"].Name);
+            Assert.AreEqual("Porthos", createdDictionary["muskateerTwo"].Name);
+            Assert.AreEqual("Aramis", createdDictionary["muskateerThree"].Name);
+        }
+    }
+}
