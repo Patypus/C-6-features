d08bc5dae5eafbc080953a3b1fe58f0098974f7d
Pat Howell <patrick.howell@outlook.com>
2015-08-10 21:46:45 +0100

    Added expression body examples.

---
+++ Features/ExpressionBodies/BodiedFunctionCallingTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Features.ExpressionBodies
+{
+    [TestFixture]
+    public class BodiedFunctionCallingTests
+    {
+        [Test]
+        public void BodiedGetterReturnsUpdatedValue()
+        {
+            var now = DateTime.Now;
+            var testItem = new BodiedFunctions(now, "Phillip");
+
+            var result = testItem.FiveDaysLater;
+            Assert.AreEqual(now.AddDays(5), result);
+        }
+
+        [Test]
+        public void BodiedFunctionReturnsFormattedString()
+        {
+            var testData = new BodiedFunctions(DateTime.Now, "Bernard");
+
+            var result = testData.GetNameInMessage();
+            Assert.AreEqual("Welcome Bernard. Have a nice day!", result);
+        }
+    }
+}
---
+++ Features/ExpressionBodies/BodiedFunctions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Features.ExpressionBodies
+{
+    public class BodiedFunctions
+    {
+        private string heldName;
+        public DateTime InitalDate { get; }
+        //Get for a property with a expression as part of the getter declaration
+        public DateTime FiveDaysLater => InitalDate.AddDays(5);
+        
+        public BodiedFunctions(DateTime date, string name)
+        {
+            //reuse of an AutoProperty initialiser!
+            InitalDate = date;
+            heldName = name;
+        }
+
+        /*
+        * Two points here, fisrtly a reuse of the new string interpolation. Next is the () in the name which
+        * marks this as a method instead of a bodied getter.
+        */
+        public string GetNameInMessage() => $"Welcome {heldName}. Have a nice day!";
+    }
+}
