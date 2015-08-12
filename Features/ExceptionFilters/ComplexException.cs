190bfe5de02a5a14f0fa5c9aeb485e98833b9c36
Pat Howell <patrick.howell@outlook.com>
2015-08-12 21:45:35 +0100

    Added complex exception type for use in exception filtering examples.

---
+++ Features/ExceptionFilters/ComplexException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Features.ExceptionFilters
+{
+    public class ComplexException : Exception
+    {
+        /*
+        * This expection type reuses the auto property setup for its contained details.
+        */
+        public string AdditionalInfo { get; }
+        public IEnumerable<Exception> NestedExceptions { get; } = new List<Exception>();
+
+        public ComplexException(string message, string otherDetails, IEnumerable<Exception> nestedExcpetions) : base(message)
+        {
+            AdditionalInfo = otherDetails;
+            NestedExceptions = nestedExcpetions;
+        }
+    }
+}
--- Features/NameOf/VerboseMessageProvider.cs
+++ Features/NameOf/VerboseMessageProvider.cs
@@ -6,6 +6,6 @@
         {
             var nameOfParameter = nameof(passedMessage);
             return $"Value of {nameOfParameter} passed to the method was {passedMessage}";
-        }
+        }        
     }
 }
