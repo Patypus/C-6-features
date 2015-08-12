d3aa076d5a0dfc5faefbe265dd46adab3e32efce
Pat Howell <patrick.howell@outlook.com>
2015-08-12 21:18:45 +0100

    Added basics of the nameof feature

---
+++ Features/NameOf/NameOfTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using static Features.NameOf.VerboseMessageProvider;
+
+namespace Features.NameOf
+{
+    [TestFixture]
+    public class NameOfTests
+    {
+        [Test]
+        public void NameOfParameterIncludedInMessage()
+        {
+            var paramValue = "'to be passed in'";
+            var result = GetMessageWithDetailsOfImplementation(paramValue);
+
+            Assert.AreEqual("Value of passedMessage passed to the method was " + paramValue, result);
+        }
+    }
+}
---
+++ Features/NameOf/VerboseMessageProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Features.NameOf
+{
+    public static class VerboseMessageProvider
+    {
+        public static string GetMessageWithDetailsOfImplementation(string passedMessage)
+        {
+            var nameOfParameter = nameof(passedMessage);
+            return $"Value of {nameOfParameter} passed to the method was {passedMessage}";
+        }
+    }
+}
